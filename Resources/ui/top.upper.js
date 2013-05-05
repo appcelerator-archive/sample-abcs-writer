909b1ace3b1b6e193f18198cf6be9b4dee199c9c
Dawson Toth <dtoth@appcelerator.com>
2013-05-05 15:41:59 -0500

    Swap Upper and Lower

--- Resources/ui/top.upper.js
+++ Resources/ui/top.upper.js
@@ -1,6 +1,4 @@
-var ColorPicker = require('./colorPicker'),
-	PaintView = require('./paintView'),
-	E = require('lib/events');
+var E = require('lib/events');
 
 /*
  Public API.
@@ -10,125 +8,63 @@ exports.addToView = addToView;
 /*
  Implementation.
  */
+
+/**
+ * Draw the bottom guide-selection bar. This will be a scroll view of letters the user can choose from.
+ * @param win
+ */
 function addToView(win) {
-	/**
-	 * Draw the top bar. This is where the user can choose size color and eraser mode, as well as saving and clearing.
-	 */
-	var topBar = Ti.UI.createView({
+	win.add(Ti.UI.createView({
 		top: 0, right: 0, left: 0,
-		height: 44,
-		backgroundImage: '/Images/Tile-Top.png'
-	});
-	win.add(topBar);
-
-	/**
-	 * Create a save button. This will save the drawing to the user's photo gallery.
-	 */
-	var save = Ti.UI.createButton({
-		width: 56, height: 31,
-		left: 5, top: 7,
-		backgroundImage: '/Images/Buttons/Save.png'
-	});
-	save.addEventListener('click', E.curryFireEvent('paint:save'));
-	topBar.add(save);
-
-	/**
-	 * Create a color button.
-	 */
-	var colorSwatch = Ti.UI.createView({
-		width: 30, height: 26,
-		left: 68, top: 9,
-		backgroundColor: PaintView.strokeColor()
-	});
-	topBar.add(colorSwatch);
-	var color = Ti.UI.createButton({
-		width: 34, height: 30,
-		left: 66, top: 7,
-		backgroundImage: '/Images/Buttons/Insert-Off.png'
-	});
-	color.addEventListener('click', function() {
-		ColorPicker.show({
-			win: win,
-			initial: colorSwatch.backgroundColor,
-			success: function(color) {
-				PaintView.strokeColor(colorSwatch.backgroundColor = color);
-			}
-		});
-	});
-	topBar.add(color);
-
-	/**
-	 * Create a size slider.
-	 */
-	var sizeSlider = Ti.UI.createSlider({
-		min: 0.25, max: 50, value: PaintView.strokeWidth(),
-		left: 105, right: 105, top: 10,
-		height: Ti.UI.SIZE
-	});
-	topBar.add(sizeSlider);
-	sizeSlider.addEventListener('change', function(e) {
-		try {
-			PaintView.strokeWidth(e.value);
-		}
-		catch (err) {
-		}
-	});
-
-	/**
-	 * Create a erase mode button.
-	 */
-	topBar.add(Ti.UI.createView({
-		width: 30, height: 26,
-		right: 68, top: 9,
-		backgroundColor: '#fff'
+		height: 38,
+		backgroundImage: '/Images/Tile-Bottom-Off.png'
 	}));
-	var eraseModeOn = false,
-		eraseMode = Ti.UI.createButton({
-			width: 34, height: 30,
-			right: 66, top: 7,
-			backgroundImage: '/Images/Buttons/Insert-Off.png',
-			opacity: 0.5
+	var bottomBar = Ti.UI.createScrollView({
+		top: 0, right: 0, left: 0,
+		height: 38,
+		contentWidth: 'auto',
+		scrollType: 'horizontal',
+		showHorizontalScrollIndicator: false,
+		horizontalBounce: true
+	});
+	win.add(bottomBar);
+
+	bottomBar.addEventListener('click', clickLetter);
+
+	var letters = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+		lastLetter = Ti.App.Properties.getString('lastLetter', ' ');
+	for (var k = 0, kL = letters.length; k < kL; k++) {
+		var label = Ti.UI.createLabel({
+			text: letters[k], textAlign: 'center',
+			top: 0, left: (40 * k),
+			height: 38,
+			font: { fontWeight: 'bold', fontSize: 20  },
+			color: '#fff',
+			width: 40
 		});
-	eraseMode.add(Ti.UI.createImageView({
-		width: 23, height: 22,
-		right: 5, top: 4,
-		image: '/Images/Buttons/Eraser.png'
-	}));
-	eraseMode.addEventListener('click', function() {
-		eraseModeOn = !eraseModeOn;
-		eraseMode.opacity = eraseModeOn ? 1 : 0.5;
-		eraseMode.backgroundImage = '/Images/Buttons/Insert-' + (eraseModeOn ? 'On' : 'Off') + '.png';
-		E.fireEvent('paint:erase-' + (eraseModeOn ? 'on' : 'off'));
-	});
-	topBar.add(eraseMode);
-
-	/**
-	 * Create a clear button.
-	 */
-	var clear = Ti.UI.createButton({
-		width: 56, height: 31,
-		right: 5, top: 7,
-		backgroundImage: '/Images/Buttons/Clear.png',
-		opacity: 0.3
-	});
-	clear.addEventListener('click', doClear);
-	topBar.add(clear);
-
-	function doClear() {
-		if (clear.undoMode) {
-			E.fireEvent('paint:swap-cleared');
+		if (Ti.Platform.osname != 'android') {
+			label.opacity = 0.5;
 		}
-		else {
-			E.fireEvent('paint:clear');
-			clear.backgroundImage = '/Images/Buttons/Undo.png';
-			clear.undoMode = true;
+		bottomBar.add(label);
+		if (letters[k] == lastLetter) {
+			label.fireEvent('click', { source: label });
+			bottomBar.scrollTo(k * 40, 0);
 		}
 	}
+}
 
-	E.addEventListener('paint:first-draw', function() {
-		clear.backgroundImage = '/Images/Buttons/Clear.png';
-		clear.undoMode = false;
-		clear.opacity = 1;
-	});
-
+var lastLabel = null;
+function clickLetter(evt) {
+	if (lastLabel != null) {
+		if (Ti.Platform.osname != 'android') {
+			lastLabel.opacity = 0.5;
+		}
+		lastLabel.backgroundImage = null;
+	}
+	lastLabel = evt.source;
+	if (Ti.Platform.osname != 'android') {
+		lastLabel.opacity = 1;
+	}
+	lastLabel.backgroundImage = '/Images/Tile-Bottom-On.png';
+	E.fireEvent('switchToLetter', lastLabel.text);
 }
